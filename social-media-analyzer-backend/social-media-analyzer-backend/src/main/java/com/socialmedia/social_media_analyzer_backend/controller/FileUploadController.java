82642a6ba308d313e9276c960c9be5a88726f692
Viraj <145934951+virajmahagavakar@users.noreply.github.com>
2025-11-15 15:33:04 +0530

    Update CORS origin for FileUploadController

--- social-media-analyzer-backend/social-media-analyzer-backend/src/main/java/com/socialmedia/social_media_analyzer_backend/controller/FileUploadController.java
+++ social-media-analyzer-backend/social-media-analyzer-backend/src/main/java/com/socialmedia/social_media_analyzer_backend/controller/FileUploadController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://virajsocialmediaanalyzer.netlify.app")
 public class FileUploadController {
 
     private final FileService fileService;
